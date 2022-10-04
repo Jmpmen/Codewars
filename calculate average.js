6050b57fe35024741fd6673a659d3937a638eefc
Jmpmen <mendozajamesmichael@gmail.com>
2022-10-04 10:17:21 +0800

    Calculate the average of the numbers in a given list

---
+++ calculate average.js
@@ -0,0 +1,5 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+const findAverage = array => array.length !== 0 ? array.reduce((acc,c) => acc+c,0) / array.length : 0
