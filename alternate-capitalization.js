feb54054604d154b3cbcdc912cdbb7e1bf32f0a7
Jmpmen <mendozajamesmichael@gmail.com>
2023-01-03 21:18:15 +0800

    added new challenges

---
+++ alternate-capitalization.js
@@ -0,0 +1,11 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+function capitalize(s){
+    return [s.split('').map((x,i) => i % 2 ? x.toLowerCase() : x.toUpperCase()).join(''),s.split('').map((x,i) => i % 2 ? x.toUpperCase() : x.toLowerCase()).join('')];
+  };
