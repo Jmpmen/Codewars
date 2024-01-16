d765bc783353871847b5290c1b84267f79ba8a01
Jmpmen <mendozajamesmichael@gmail.com>
2024-01-16 13:23:22 +0800

    added new challenges

---
+++ alphabetical-addition.js
@@ -0,0 +1,24 @@
+// Your task is to add up letters to one letter.
+
+// The function will be given a variable amount of arguments, each one being a letter to add.
+
+// Notes:
+
+// Letters will always be lowercase.
+// Letters can overflow (see second to last example of the description)
+// If no letters are given, the function should return 'z'
+// Examples:
+
+// addLetters('a', 'b', 'c') = 'f'
+// addLetters('a', 'b') = 'c'
+// addLetters('z') = 'z'
+// addLetters('z', 'a') = 'a'
+// addLetters('y', 'c', 'b') = 'd' // notice the letters overflowing
+// addLetters() = 'z'
+
+function addLetters(...letters) {
+  if (!letters.length) return "z";
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const sum = letters.reduce((acc, c) => acc + (alphabet.indexOf(c) + 1), 0);
+  return alphabet.charAt((sum - 1) % 26);
+}
