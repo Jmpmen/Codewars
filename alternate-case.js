7e0f77ee39f97013b993849ff9cf5de8d8ac758d
Jmpmen <mendozajamesmichael@gmail.com>
2024-01-18 13:40:49 +0800

    added new challenges

---
+++ alternate-case.js
@@ -0,0 +1,12 @@
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+function alternateCase(s) {
+  return s
+    .split("")
+    .map((letter) =>
+      letter === letter.toLowerCase()
+        ? letter.toUpperCase()
+        : letter.toLowerCase()
+    )
+    .join("");
+}
